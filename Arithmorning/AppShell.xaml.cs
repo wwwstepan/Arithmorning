1b654634e9392e05df60e6b1d18368c447e5056a
Stepan Purk <63333004+wwwstepan@users.noreply.github.com>
2024-02-11 15:01:05 +0300

    Add files via upload

---
+++ Arithmorning/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Arithmorning
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Arithmorning/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Arithmorning;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        //var handler = //MainPageElement
+        //    MainScrollView.Handler;        
+        //var elem = handler?.PlatformView as IElementHandler;
+        //if (elem is not null)
+        //    elem.
+    }
+}
